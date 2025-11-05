d15f0751d4c4140355d7521da68871e40bd982cf
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-11-05 16:30:26 +0000

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
     implementation(libs.play.services.location)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/amfootball/ui/screens/Match/FinishMatch.kt
+++ app/src/main/java/com/example/amfootball/ui/screens/Match/FinishMatch.kt
@@ -1,2 +1,116 @@
 package com.example.amfootball.ui.screens.Match
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.amfootball.ui.components.BackTopBar
+import com.example.amfootball.ui.theme.AMFootballTheme
+
+//De aguma for vai ser enviado como parametro deste metodo o adversario e a equipa atual
+//Depois quando tiver isso adaptar este metodo
+@Composable
+fun FinishMatchScreen(navHostController: NavHostController) {
+    Scaffold(
+        topBar = {
+            BackTopBar(
+                navHostController = navHostController,
+                title = "Finish Match"
+            )
+        },
+        content = { paddingValues ->
+            FormFinishMatch(modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp)
+            )
+        }
+    )
+}
+
+@Composable
+private fun FormFinishMatch(modifier: Modifier = Modifier) {
+    Column(modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Finsih Match Form")
+
+
+    }
+}
+
+@Composable
+private fun TextFieldForm() {
+    var numGoalsTeam by remember { mutableStateOf("") }
+    var numGoalsAdversary by remember { mutableStateOf("") }
+
+    //Criar um fun que retorna o valueChange que está codigo repetido
+    CreateTextFieldsNumber(numGoals = numGoalsTeam,
+        onValueChange = { newText ->
+            if (newText.isEmpty()) {
+                numGoalsTeam = newText
+            }
+            else if (newText.all { it.isDigit() }) {
+                val number = newText.toIntOrNull()
+                if (number != null && number > 0 && number < 100) {
+                    numGoalsTeam = newText
+                }
+            }
+        },
+        label = "Golos da sua Equipa"
+    )
+
+    CreateTextFieldsNumber(numGoals = numGoalsAdversary,
+        onValueChange = { newText ->
+            if (newText.isEmpty()) {
+                numGoalsAdversary = newText
+            }
+            else if (newText.all { it.isDigit() }) {
+                val number = newText.toIntOrNull()
+                if (number != null && number > 0 && number < 100) {
+                    numGoalsAdversary = newText
+                }
+            }
+        },
+        label = "Golos da sua Equipa"
+    )
+
+
+}
+
+@Composable
+private fun CreateTextFieldsNumber(numGoals: String,
+                                   onValueChange: (String) -> Unit,
+                                   label: String) {
+    TextField(
+        value = numGoals,
+        onValueChange = onValueChange,
+        label = { Text(text = label) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+}
+
+
+@Preview(name= "FinishMatch EN", locale = "en", showBackground = true)
+@Preview(name= "FinishMatch PT", locale = "pt", showBackground = true)
+@Composable
+fun PreviewFinishMatch(){
+    AMFootballTheme{
+        FinishMatchScreen(navHostController = rememberNavController())
+    }
+}
