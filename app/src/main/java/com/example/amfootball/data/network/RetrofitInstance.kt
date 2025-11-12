8967e7562653f1b0d50f4a1fc8facfe3433c1387
Willkie <8230127@estg.ipp.pt>
2025-11-12 16:08:09 +0000

    Update RetrofitInstance.kt

--- app/src/main/java/com/example/amfootball/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/amfootball/data/network/RetrofitInstance.kt
@@ -8,7 +8,7 @@ object RetrofitInstance {
 
     // MUDE ISTO para o URL base da sua API .NET
     //private const val BASE_URL = "http:192.168.196.1" // link com ngrok
-    private const val BASE_URL = "https://thrillful-temika-postlicentiate.ngrok-free.dev " // link com ngrok
+    private const val BASE_URL = "https://amfootballapi.duckdns.org" // link com ngrok
 
     //Cria o cliente aqui
     private val okHttpClient = OkHttpClient.Builder()
