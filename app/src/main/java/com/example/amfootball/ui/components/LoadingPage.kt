7df450082ca85ea426f7d1b1f04a7ffb7bed6fbe
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-11-21 10:32:42 +0000

    .

--- app/src/main/java/com/example/amfootball/ui/components/LoadingPage.kt
+++ app/src/main/java/com/example/amfootball/ui/components/LoadingPage.kt
@@ -10,6 +10,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.amfootball.R
 
 @Composable
 fun LoadingPage(
@@ -50,6 +52,8 @@ fun RetryButton(
 ) {
     Column {
         Text(text = errorMsg!!, color = Color.Red)
-        Button(onClick = retry) { Text("Tentar Novamente") }
+        Button(onClick = retry) {
+            Text(text = stringResource(id = R.string.button_try_again))
+        }
     }
 }
