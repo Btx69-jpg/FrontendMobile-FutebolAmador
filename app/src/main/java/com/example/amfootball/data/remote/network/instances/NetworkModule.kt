a051f2b739ff875860701cb73594cb42348f3a19
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-12-14 21:58:17 +0000

    Update NetworkModule.kt

--- app/src/main/java/com/example/amfootball/data/remote/network/instances/NetworkModule.kt
+++ app/src/main/java/com/example/amfootball/data/remote/network/instances/NetworkModule.kt
@@ -72,7 +72,7 @@ object NetworkModule {
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(NetworkConsts.BASE_URL_NGROOK)
+            .baseUrl(NetworkConsts.BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
